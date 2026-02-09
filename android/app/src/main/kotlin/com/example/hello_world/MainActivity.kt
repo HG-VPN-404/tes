e7be437996d44b316c76e7d2d2b56c1b2185a005
HG-VPN-404 <hpperdana72+ha@gmail.com>
2026-02-09 16:15:14 +0700

    update file

--- android/app/src/main/kotlin/com/example/hello_world/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hello_world/MainActivity.kt
@@ -1,13 +1,7 @@
 package com.example.hello_world
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+    // Kosong saja, biarkan Flutter V2 yang menangani semuanya
 }
